faeef3dd9f8892325bd4d77a4f35e254ba3bc666
ClientbasePro <42553531+ClientbasePro@users.noreply.github.com>
2018-12-23 16:46:13 +0300

    добавлены новые функции

--- Samples1.js
+++ Samples1.js
@@ -8,11 +8,45 @@ display_field({$one_field.id}, $('#view_cell_{$one_field.id}').text()!="0");
 display_field({$one_field.id}, $('#view_cell_{$one_field.id}').text()!="0,00");
 
 
+    // АДРЕС
+$("#value690").suggestions({
+    serviceUrl: "https://suggestions.dadata.ru/suggestions/api/4_1/rs",
+    token: "2b5decc3e9a21ace7436634495bcb163ac9f6412",
+    type: "ADDRESS",
+    count: 5,
+    formatSelected: function (suggestions) { return suggestions.value || ""; }
+});
 
+	// БАНК
+$("#value4967").suggestions({
+    serviceUrl: "https://suggestions.dadata.ru/suggestions/api/4_1/rs",
+    token: "d98dd56324086753fcd28fe72d3168420d816325",
+    type: "BANK",
+    count: 5,
+    formatSelected: function (suggestion) { 
+        if ((el=document.getElementById("value4957")) && suggestion.data.name && suggestion.data.name.short) el.value = suggestion.data.name.short;
+        if ((el=document.getElementById("value4977")) && suggestion.data.correspondent_account) el.value = suggestion.data.correspondent_account;
+        return suggestion.data.bic || ""; 
+    }
+});
 
-
-
-
+    // РЕКВИЗИТЫ
+$("#value780").suggestions({
+    serviceUrl: "https://suggestions.dadata.ru/suggestions/api/4_1/rs",
+    token: "2b5decc3e9a21ace7436634495bcb163ac9f6412",
+    type: "PARTY",
+    count: 5,
+    formatSelected: function (suggestion) {  
+        if (document.getElementById("value790") && suggestion.data.address.unrestricted_value) document.getElementById("value790").value = suggestion.data.address.unrestricted_value;
+        if (document.getElementById("value690") && suggestion.data.address.unrestricted_value) document.getElementById("value690").value = suggestion.data.address.unrestricted_value;
+        if (document.getElementById("value800") && suggestion.data.ogrn) document.getElementById("value800").value = suggestion.data.ogrn;
+        if (document.getElementById("value810") && suggestion.data.inn) document.getElementById("value810").value = suggestion.data.inn;
+        if (document.getElementById("value660") && suggestion.unrestricted_value) document.getElementById("value660").value = suggestion.unrestricted_value;
+        if (document.getElementById("value820") && suggestion.data.kpp) document.getElementById("value820").value = suggestion.data.kpp;
+        if (document.getElementById("value880") && suggestion.data.management && suggestion.data.management.name) document.getElementById("value880").value = suggestion.data.management.name;
+        return suggestion.unrestricted_value || "";
+    }
+});
 
 
 
--- Samples1.php
+++ Samples1.php
@@ -5,16 +5,68 @@
   
   
   
+  // форматирование поля для телефона
+$p = explode(',', $line['Телефон']);
+foreach ($p as $phone) $phones[] = ($p_=SetNumber($phone)) ? $p_ : $phone;
+$line['Телефон'] = implode(',', $phones);  
+
+  
+  // уведомление аналог JS alert, работающее даже в подтаблице
+calc_alerts($text);
+
+
+  // суммирование из подтаблицы
+if ('delete'==$event['type'] || $line['Статус записи']) $c = " AND id!='".$ID."' ";
+$row = sql_fetch_assoc(data_select_field(550, 'SUM(f7460) AS sum', "status=0 {$c} AND f9430='".$line['Счёт']['raw']."'"));
+$line['Счёт']['Оплачено'] = $row['sum'];  
   
   
+  // часть 1 Отчётов с выбором периода МГ - МГ
+$months = array('Январь'=>1,'Февраль'=>2,'Март'=>3,'Апрель'=>4,'Май'=>5,'Июнь'=>6,'Июль'=>7,'Август'=>8,'Сентябрь'=>9,'Октябрь'=>10,'Ноябрь'=>11,'Декабрь'=>12);
+$months_ = array_flip($months);
+$m1 = ($m1=intval($_REQUEST['m1'])) ? $m1 : 1;
+$y1 = ($y1=intval($_REQUEST['y1'])) ? $y1 : date("Y");
+$m2 = ($m2=intval($_REQUEST['m2'])) ? $m2 : intval(date("m"));
+$y2 = ($y2=intval($_REQUEST['y2'])) ? $y2 : date("Y");
+$m = 1;
+while ($m<13) { $mList1 .= '<option value="'.$m.'" '.(($m==$m1)?" selected ":"").'>'.$months_[$m].'</option>'; $m++; }
+$m = 1;
+while ($m<13) { $mList2 .= '<option value="'.$m.'" '.(($m==$m2)?" selected ":"").'>'.$months_[$m].'</option>'; $m++; }
+$y = 2017;
+while ($y<2026) { $yList1 .= '<option '.(($y==$y1)?" selected ":"").'>'.$y.'</option>'; $y++; }
+$y = 2017;
+while ($y<2026) { $yList2 .= '<option '.(($y==$y2)?" selected ":"").'>'.$y.'</option>'; $y++; }
+$smarty->assign("mList1", $mList1);
+$smarty->assign("mList2", $mList2);
+$smarty->assign("yList1", $yList1);
+$smarty->assign("yList2", $yList2);
   
-  
-  
-  
-  
-  
-  
-  
+  // часть 2
+<div class="reportFilters">
+  <table style="margin: 0px auto;">
+    <tr>
+      <td>        
+        <span>Период с <select name="m1" onchange="document.getElementById('report_form').submit();return false;">{$mList1}</select> <select name="y1" onchange="document.getElementById('report_form').submit();return false;">{$yList1}</select></span>
+      </td>
+      <td>
+        <span>по <select name="m2" onchange="document.getElementById('report_form').submit();return false;">{$mList2}</select> <select name="y2" onchange="document.getElementById('report_form').submit();return false;">{$yList2}</select></span>
+      </td>
+    </tr>
+  </table>
+</div>  
+
+
+
+
+
+
+
+
+
+
+
+
+
   
   
   
