36f5a2cc37d158a2ef805579c518524c6f7445d1
ClientbasePro <42553531+ClientbasePro@users.noreply.github.com>
2018-12-23 14:46:26 +0300

    добавил Samples1.js

---
+++ Samples1.js
@@ -0,0 +1,26 @@
+  // примеры кода для CRM Clientbase
+
+  
+  
+  // скрытие пустого поля в карточке средствами JS
+display_field({$one_field.id}, $('#view_cell_{$one_field.id}').text()!="");
+display_field({$one_field.id}, $('#view_cell_{$one_field.id}').text()!="0");
+display_field({$one_field.id}, $('#view_cell_{$one_field.id}').text()!="0,00");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
